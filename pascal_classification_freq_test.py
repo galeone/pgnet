7c80c4539fa43b9752d9b14044363beeccb5ddb9
Paolo Galeone <nessuno@nerdz.eu>
2016-09-15 13:00:47 +0200

    classification via localization

--- pascal_classification_freq_test.py
+++ pascal_classification_freq_test.py
@@ -10,16 +10,15 @@
 import argparse
 import os
 import sys
-import time
 import math
 from collections import defaultdict
+from PIL import Image
 import tensorflow as tf
 import cv2
 import numpy as np
 import train
 import pgnet
 import pascal_input
-import image_processing
 
 # pascal sorted labels
 PASCAL_LABELS = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
@@ -195,8 +194,6 @@ def group_overlapping_with_same_class(map_of_regions):
         rect_list, _ = cv2.groupRectangles(
             rects_only.tolist(), 1, eps=RECT_SIMILARITY)
 
-        tot = len(rect_list)
-
         if len(rect_list) > 0:
             # for every merged rectangle, calculate the avg prob and
             # the number of intersection
@@ -222,8 +219,8 @@ def group_overlapping_with_same_class(map_of_regions):
 def main(args):
     """ main """
 
-    if not os.path.exists(args.image_path):
-        print("{} does not exists".format(args.image_path))
+    if not os.path.exists(args.test_ds):
+        print("{} does not exists".format(args.test_ds))
         return 1
 
     current_dir = os.path.abspath(os.getcwd())
@@ -236,6 +233,11 @@ def main(args):
     # export model.pb from session dir. Skip if model.pb already exists
     pgnet.export_model(num_classes, current_dir + "/session", "model-0",
                        "model.pb")
+
+    results_dir = "{}/results".format(current_dir)
+    files = {label: open(
+        results_dir + "/VOC2012/Main/comp1_cls_test_{}.txt".format(label), "w")
+             for label in PASCAL_LABELS}
 
     with tf.Graph().as_default() as graph, tf.device(args.device):
         const_graph_def = tf.GraphDef()
@@ -261,150 +263,127 @@ def main(args):
         top_k = tf.nn.top_k(per_region_probabilities, k=num_classes)
         # each with shape [tested_positions, k]
 
-        original_image = tf.image.convert_image_dtype(
-            image_processing.read_image(
-                tf.constant(args.image_path), 3,
-                args.image_path.split('.')[-1]),
-            dtype=tf.uint8)
-        original_image_dim = tf.shape(original_image)
-
         k = 2
-        eval_image_side = tf.cond(
-            tf.less_equal(
-                tf.minimum(original_image_dim[0], original_image_dim[1]),
-                tf.constant(pgnet.INPUT_SIDE)),
-            lambda: tf.constant(pgnet.INPUT_SIDE),
-            lambda: tf.constant(pgnet.INPUT_SIDE + pgnet.DOWNSAMPLING_FACTOR * pgnet.LAST_CONV_INPUT_STRIDE * k))
+        input_side = pgnet.INPUT_SIDE + pgnet.DOWNSAMPLING_FACTOR * pgnet.LAST_CONV_INPUT_STRIDE * k
 
-        eval_image = tf.expand_dims(
-            image_processing.zm_mp(
-                image_processing.resize_bl(original_image, eval_image_side)),
-            0)
+        test_queue, test_filename_queue = pascal_input.test(
+            args.test_ds, 29, input_side,
+            args.test_ds + "/ImageSets/Main/test.txt")
+
+        init_op = tf.group(tf.initialize_all_variables(),
+                   tf.initialize_local_variables())
 
         with tf.Session(config=tf.ConfigProto(
                 allow_soft_placement=True)) as sess:
 
-            input_image, input_image_side, image = sess.run(
-                [eval_image, eval_image_side, original_image])
+            sess.run(init_op)
+            coordinator = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(
+                sess=sess, coord=coordinator)
 
-            start = time.time()
-            probability_map, top_values, top_indices = sess.run(
-                [logits, top_k[0], top_k[1]], feed_dict={images_: input_image})
+            try:
+                processed = 0
+                while not coordinator.should_stop():
+                    image_batch, filename_batch = sess.run(
+                        [test_queue, test_filename_queue])
 
-            # let's think to the net as a big net, with the last layer (before the FC
-            # layers for classification) with a receptive field of
-            # LAST_KERNEL_SIDE x LAST_KERNEL_SIDE. Lets approximate the net with this last kernel:
-            # If the image is scaled down to  LAST_KERNEL_SIDE x LAST_KERNEL_SIDE
-            # the output is a single point.
-            # if the image is scaled down to something bigger
-            # (that make the output side of contolution integer) the result is a spacial map
-            # of points. Every point has a depth of num classes.
+                    probability_map, top_values, top_indices = sess.run(
+                        [logits, top_k[0], top_k[1]],
+                        feed_dict={images_: image_batch})
 
-            # for every image in the input batch
-            for _ in range(len(input_image)):
-                # scaling factor between original image and resized image
-                full_image_scaling_factors = np.array(
-                    [image.shape[1] / input_image_side,
-                     image.shape[0] / input_image_side])
+                    # let's think to the net as a big net, with the last layer (before the FC
+                    # layers for classification) with a receptive field of
+                    # LAST_KERNEL_SIDE x LAST_KERNEL_SIDE. Lets approximate the net with this last kernel:
+                    # If the image is scaled down to  LAST_KERNEL_SIDE x LAST_KERNEL_SIDE
+                    # the output is a single point.
+                    # if the image is scaled down to something bigger
+                    # (that make the output side of contolution integer) the result is a spacial map
+                    # of points. Every point has a depth of num classes.
 
-                # shape of the rectangle analyzed in the original image
-                analyzed_shape = np.array([image.shape[1], image.shape[0]]) * (
-                    math.sqrt(pgnet.INPUT_SIDE *
-                              pgnet.LAST_KERNEL_SIDE) / input_image_side)
+                    # for every image in the input batch
+                    for batch_elem_id in range(len(image_batch)):
+                        # scaling factor between original image and resized image
+                        decoded_filename = filename_batch[
+                            batch_elem_id].decode("utf-8")
+                        image = Image.open(args.test_ds + "/JPEGImages/" +
+                                           decoded_filename + ".jpg")
+                        full_image_scaling_factors = np.array(
+                            [image.size[0] / input_side,
+                             image.size[1] / input_side])
 
-                probability_coords = 0
-                glance = defaultdict(list)
-                # select count(*), avg(prob) from map group by label, order by count, avg.
-                group = defaultdict(lambda: defaultdict(float))
-                for pmap_y in range(probability_map.shape[1]):
-                    # calculate position in the downsampled image ds
-                    ds_y = pmap_y * pgnet.LAST_CONV_OUTPUT_STRIDE
-                    for pmap_x in range(probability_map.shape[2]):
-                        ds_x = pmap_x * pgnet.LAST_CONV_OUTPUT_STRIDE
+                        probability_coords = 0
+                        glance = defaultdict(list)
+                        # select count(*), avg(prob) from map group by label, order by count, avg.
+                        group = defaultdict(lambda: defaultdict(float))
+                        for pmap_y in range(probability_map.shape[1]):
+                            # calculate position in the downsampled image ds
+                            ds_y = pmap_y * pgnet.LAST_CONV_OUTPUT_STRIDE
+                            for pmap_x in range(probability_map.shape[2]):
+                                ds_x = pmap_x * pgnet.LAST_CONV_OUTPUT_STRIDE
 
-                        if top_indices[probability_coords][
-                                0] != BACKGROUND_CLASS:
+                                if top_indices[probability_coords][
+                                        0] != BACKGROUND_CLASS:
 
-                            # create coordinates of rect in the downsampled image
-                            # convert to numpy array in order to use broadcast ops
-                            coord = [ds_x, ds_y, ds_x + pgnet.LAST_KERNEL_SIDE,
-                                     ds_y + pgnet.LAST_KERNEL_SIDE]
-                            # if something is found, append rectagle to the
-                            # map of rectalges per class
-                            rect = upsample_and_shift(
-                                coord, pgnet.DOWNSAMPLING_FACTOR, [0, 0],
-                                full_image_scaling_factors)
+                                    # create coordinates of rect in the downsampled image
+                                    # convert to numpy array in order to use broadcast ops
+                                    coord = [ds_x, ds_y,
+                                             ds_x + pgnet.LAST_KERNEL_SIDE,
+                                             ds_y + pgnet.LAST_KERNEL_SIDE]
+                                    # if something is found, append rectagle to the
+                                    # map of rectalges per class
+                                    rect = upsample_and_shift(
+                                        coord, pgnet.DOWNSAMPLING_FACTOR,
+                                        [0, 0], full_image_scaling_factors)
 
-                            prob = top_values[probability_coords][0]
-                            label = PASCAL_LABELS[top_indices[
-                                probability_coords][0]]
+                                    prob = top_values[probability_coords][0]
+                                    label = PASCAL_LABELS[top_indices[
+                                        probability_coords][0]]
 
-                            rect_prob = [rect, prob]
-                            glance[label].append(rect_prob)
-                            group[label]["count"] += 1
-                            group[label]["prob"] += prob
+                                    rect_prob = [rect, prob]
+                                    glance[label].append(rect_prob)
+                                    group[label]["count"] += 1
+                                    group[label]["prob"] += prob
 
-                        # update probability coord value
-                        probability_coords += 1
+                                # update probability coord value
+                                probability_coords += 1
 
-                classes = group.keys()
-                print('Found {} classes: {}'.format(len(classes), classes))
+                        classes = group.keys()
+                        print('Found {} classes: {}'.format(
+                            len(classes), classes))
 
-                # find out the minimum amount of intersection among regions
-                # in the original image, that can be used to trigger a match
-                looked_pos_side = probability_map.shape[
-                    1]  # or 2, is s square. 0 dim is batch
-                looked_pos = looked_pos_side**2
-                print(looked_pos_side, looked_pos)
-                min_img_orig_side = min([image.shape[1], image.shape[0]])
-                min_analyzed_region_side = min(analyzed_shape)
-                overlap_amount = math.floor(
-                    (looked_pos_side * min_analyzed_region_side -
-                     min_img_orig_side) / looked_pos_side) - 1
+                        # find out the minimum amount of intersection among regions
+                        # in the original image, that can be used to trigger a match
+                        looked_pos_side = probability_map.shape[
+                            1]  # or 2, is s square. 0 dim is batch
+                        looked_pos = looked_pos_side**2
 
-                min_intersection = math.floor(overlap_amount / 2)
-                print('min intersection: ', min_intersection)
+                        # Save the relative frequency for every class
+                        # To trigger a match, at least a fraction of intersection should be present
+                        for label in group:
+                            group[label]["rf"] = group[label][
+                                "count"] / looked_pos
+                            files[label].write("{} {}\n".format(
+                                decoded_filename, group[label]["rf"]))
+                        for label in set(set(PASCAL_LABELS) - group.keys()):
+                            files[label].write("{} {}\n".format(
+                                decoded_filename, 0))
 
-                # Save the relative frequency for every class
-                # To trigger a match, at least a fraction of intersection should be present
-                for label in group:
-                    group[label]["prob"] /= group[label]["count"]
-                    group[label]["rf"] = group[label]["count"] / looked_pos
-                    print(label, group[label])
+                        processed += 1
 
-                # merge overlapping rectangles for each class.
-                # return a map of {"label": [rect, prob, count]
-                localize = group_overlapping_with_same_class(glance)
-                end_time = time.time() - start
-                print("time: {}".format(end_time))
-                print(localize)
+            except tf.errors.OutOfRangeError:
+                print("[I] Done. Test completed!")
+                print("Processed {} images".format(processed))
+            finally:
+                coordinator.request_stop()
 
-                # now I can convert RGB to BGR to display image with OpenCV
-                # I can't do that before, because ROIs gets extracted on RGB image
-                # in order to be processed without errors by Tensorflow
-                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                for label, rect_prob_list in localize.items():
-                    for rect_prob in rect_prob_list:
-                        rect = rect_prob[0]
-                        prob = rect_prob[1]
-                        count = rect_prob[2]
-                        freq = group[label]["rf"]
-                        if count >= min_intersection and freq > 0.1:
-                            draw_box(
-                                image,
-                                rect,
-                                "{}({:.3})".format(label, prob),
-                                LABEL_COLORS[label],
-                                thickness=2)
+            coordinator.join(threads)
 
-                cv2.imshow("img", image)
-                cv2.waitKey(0)
-                return 0
+        for label in files:
+            files[label].close()
 
 
 if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser(
-        description="Apply the model to image-path")
+    PARSER = argparse.ArgumentParser(description="Test the model")
     PARSER.add_argument("--device", default="/gpu:1")
-    PARSER.add_argument("--image-path")
+    PARSER.add_argument("--test-ds")
     sys.exit(main(PARSER.parse_args()))
